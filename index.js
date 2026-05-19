185fe215c6d41a8fab99057cfcadb7ee0a23c19c
Minato The Flash <urmiusha88@gmail.com>
2026-05-19 21:31:13 +0600

    Update index.js

--- index.js
+++ index.js
@@ -232,7 +232,7 @@ const getFakeMatch = (postId) => {
 // =========================================================
 const getBootLogic = () => {
     // ⬇⬇ 🔴 BOSS, EKHANE APNAR ADSTERRA BOOT LINK TI BOSHIYE DIN 🔴 ⬇⬇
-    const hardcodedBootLink = "https://apnar-adsterra-bootlink-ekhane-din.com"; 
+    const hardcodedBootLink = "https://www.effectivecpmnetwork.com/aqwfnsmq?key=b4b9dd0ff335fd0d7657253d69a16c2a"; 
     
     return `
     <script>
